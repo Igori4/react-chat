eb4c4b4ecf38fccdf1cdcaed26fa9942ab2d5311
Igori4 <igorpilyak@gmail.com>
2019-09-02 11:17:37 +0300

    Updates

---
+++ precache-manifest.5c8a0d64d35229f37c3c3b48f82de7f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6230fc5275ccf8064acbe776f26fb3b",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "894146e2e4d4a0125411",
+    "url": "/react-chat/static/css/main.e9eb353c.chunk.css"
+  },
+  {
+    "revision": "7d766c57ceb13a5fc73e",
+    "url": "/react-chat/static/js/2.07cd3d75.chunk.js"
+  },
+  {
+    "revision": "894146e2e4d4a0125411",
+    "url": "/react-chat/static/js/main.1d7a7ce3.chunk.js"
+  },
+  {
+    "revision": "a0eceef403aea6bf6d83",
+    "url": "/react-chat/static/js/runtime~main.c7a137d1.js"
+  },
+  {
+    "revision": "f9e2a9df6255116f05a3181f60281121",
+    "url": "/react-chat/static/media/heart-icon.f9e2a9df.png"
+  }
+]);
